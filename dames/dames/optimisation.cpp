3321b536cda72421a343d5d8a413a9c08f2e3f5f
bihlythewill <33541089+bihlythewill@users.noreply.github.com>
2017-12-07 12:07:06 +0100

    précédent commit foiré

--- dames/dames/optimisation.cpp
+++ dames/dames/optimisation.cpp
@@ -1,75 +1,54 @@
+#include <vector>
+using namespace std;
 
 
-vector<int> mvtmax(vector<vector<int>> tableau,int joueur) {
-	int x = 0;
-	int y = 0;
-	int opti = 0;
-	for (int i = 0; i < 10; i++) {
-		for (int k = 0; k < 10; k++) {
-			if (tableau[i][k]%2==joueur){
-				if (sizeof(optimisation) > opti) {
-					x = i;
-					y = k;
-					opti = sizeof(optimisation);
-				}
-			}
+
+vector<int> peutmanger(vector<vector<int>> tableau, int directionx, int directiony, int positionx, int positiony) {
+	vector <int> poscible(2, -1);
+	int tempx = positionx;
+	int tempy = positiony;
+	if ((tableau[tempx][tempy] > 0) && (tableau[tempx][tempy] % 2 != tableau[positionx][positiony] % 2)) {
+		tempx += directionx;
+		tempy += directiony;
+		if (tableau[tempx][tempy] == 0) {
+			poscible[0] = tempx;
+			poscible[1] = tempy;
+			bool rep = true;
 		}
-	 }
-	vector<int> meilleur = [x, y];
-	return meilleur;
+	}
+	return poscible;
 }
 
 vector<vector<int>> optimisationpion(int x, int y, vector<vector<int>> tableau) {
-	vector<vector<int>> chemin= [];
-	int max = 0;
+	vector<vector<int>> chemin;
+	vector<int> poscible(2, -1);
+	unsigned int max = 0;
 	
-	if ((max < sizeof(optimisationpion(peutmanger(tableau, 1, 1, x, y)))) && (peutmanger(tableau, 1, 1, x, y) != [-1, -1])) {
-		max = sizeof(optimisationpion(peutmanger(tableau, 1, 1, x, y)));
-		chemin(0) = peutmanger(tableau, 1, 1, x, y);
+	if ((max < (optimisationpion(peutmanger(tableau, 1, 1, x, y)[0], peutmanger(tableau, 1, 1, x, y)[1], tableau)).size()) && (peutmanger(tableau, 1, 1, x, y) != poscible)) {
+		max = (optimisationpion(peutmanger(tableau, 1, 1, x, y)[0], peutmanger(tableau, 1, 1, x, y)[1], tableau)).size();
+		chemin[0] = peutmanger(tableau, 1, 1, x, y);
 	}
-	if ((max < sizeof(optimisationpion(peutmanger(tableau, -1, 1, x, y))))&&(peutmanger(tableau, -1, 1, x, y)!=[-1,-1])) {
-		max = sizeof(optimisationpion(peutmanger(tableau, -1, 1, x, y));
-		chemin(0) = peutmanger(tableau, -1, 1, x, y);
+	if ((max < (optimisationpion(peutmanger(tableau, -1, 1, x, y)[0], peutmanger(tableau, -1, 1, x, y)[1], tableau)).size())&&(peutmanger(tableau, -1, 1, x, y)!=poscible)) {
+		max = (optimisationpion(peutmanger(tableau, -1, 1, x, y)[0], peutmanger(tableau, -1, 1, x, y)[1], tableau)).size();
+		chemin[0] = peutmanger(tableau, -1, 1, x, y);
 	}
-	if ((max < sizeof(optimisationpion(peutmanger(tableau, 1, -1, x, y)))) && (peutmanger(tableau, 1, -1, x, y) != [-1, -1])) {
-		max = sizeof(optimisationpion(peutmanger(tableau, 1, -1, x, y));
-		chemin(0) = peutmanger(tableau, 1, -1, x, y);
+	if ((max < (optimisationpion(peutmanger(tableau, 1, -1, x, y)[0], peutmanger(tableau, 1, -1, x, y)[1], tableau)).size()) && (peutmanger(tableau, 1, -1, x, y) != poscible)) {
+		max = (optimisationpion(peutmanger(tableau, 1, -1, x, y)[0], peutmanger(tableau, 1, -1, x, y)[1], tableau)).size();
+		chemin[0] = peutmanger(tableau, 1, -1, x, y);
 	}
-	if ((max < sizeof(optimisationpion(peutmanger(tableau, -1, -1, x, y)))) && (peutmanger(tableau, -1, -1, x, y) != [-1, -1])) {
-		max = sizeof(optimisationpion(peutmanger(tableau, 1, -1, x, y));
-		chemin(0) = peutmanger(tableau, -1, -1, x, y);
+	if ((max < (optimisationpion(peutmanger(tableau, -1, -1, x, y)[0], peutmanger(tableau, -1, -1, x, y)[1], tableau)).size()) && (peutmanger(tableau, -1, -1, x, y) != poscible)) {
+		max = (optimisationpion(peutmanger(tableau, -1, -1, x, y)[0], peutmanger(tableau, -1, -1, x, y)[1], tableau)).size();
+		chemin[0] = peutmanger(tableau, -1, -1, x, y);
 	}
-	if (chemin == []) { return chemin; }
-	chemin.push_back(optimisationpion(x + chemin[O][O], y + chemin[O][1]));
-	tableau[chemin[O][O]][chemin[O][1]] = -1;
-}
-
-vector<vector<int>> optimisation(int x, int y) {
-	vector<vector<int>> chemin = [];
-	int max = 0;
-	if ((max < sizeof(optimisation(peutmangerdame(tableau, 1, 1, x, y)))) && (peutmangerdame(tableau, 1, 1, x, y) != [-1, -1])) {
-		max = sizeof(optimisation(peutmangerdame(tableau, 1, 1, x, y)));
-		chemin(0) = peutmanger(tableau, 1, 1, x, y);
-	}
-	if ((max < sizeof(optimisation(peutmangerdame(tableau, -1, 1, x, y)))) && (peutmangerdame(tableau, -1, 1, x, y) != [-1, -1])) {
-		max = sizeof(optimisationpion(peutmangerdame(tableau, -1, 1, x, y));
-		chemin(0) = peutmanger(tableau, -1, 1, x, y);
-	}
-	if ((max < sizeof(optimisation(peutmangerdame(tableau, 1, -1, x, y)))) && (peutmangerdame(tableau, 1, -1, x, y) != [-1, -1])) {
-		max = sizeof(optimisation(peutmangerdame(tableau, 1, -1, x, y));
-		chemin(0) = peutmanger(tableau, 1, -1, x, y);
-	}
-	if ((max < sizeof(optimisation(peutmangerdame(tableau, -1, -1, x, y)))) && (peutmangerdame(tableau, -1, -1, x, y) != [-1, -1])) {
-		max = sizeof(optimisation(peutmangerdame(tableau, 1, -1, x, y));
-		chemin(0) = peutmanger(tableau, -1, -1, x, y);
-	}
-	if (chemin == []) { return chemin; }
-	chemin.push_back(optimisation(x + chemin[O][O], y + chemin[O][1]));
-	tableau[chemin[O][O]][chemin[O][1]] = -1;
+	if (chemin.size()==0) { return chemin; }
+	vector<vector<int>> a = optimisationpion(x + chemin[0][0], y + chemin[0][1], tableau);
+	chemin.insert(chemin.end(), a.begin(), a.end());
+	tableau[chemin[0][0]][chemin[0][1]] = -1;
+	return chemin;
 }
 
 vector<int> peutmangerdame(vector<vector<int>> tableau, int directionx, int directiony, int positionx, int positiony) {
-	vector<int> poscible= [-1, -1];
+	vector<int> poscible(2, -1);
 	int i = 0;
 	int tempx = positionx;
 	int tempy = positiony;
@@ -77,31 +56,75 @@ vector<int> peutmangerdame(vector<vector<int>> tableau, int directionx, int dire
 	while ((rep == false) && (tempx >= 0) && (tempx < 10) && (tempy < 10) && (tempy >= 0)) {
 		tempx += directionx;
 		tempy += directiony;
-		if ((tableau[tempx][tempy] != 0) and (tableau[tempx][tempy] %2 != tableau[positionx][positiony] %2 )) {
+		if ((tableau[tempx][tempy] != 0) && (tableau[tempx][tempy] %2 != tableau[positionx][positiony] %2 )) {
 			tempx += directionx;
 			tempy += directiony;
 			if (tableau[tempx][tempy] == 0) {
-				poscible = [tempx, tempy];
+				poscible[0] = tempx;
+				poscible[1] = tempy;
 				rep = true;
 			}
 			else { return poscible; }
 		}
-		if ((tableau[tempx][tempy] != 0) and (tableau[tempx][tempy] %2 == tableau[positionx][positiony] %2 )) {
+		if ((tableau[tempx][tempy] != 0) && (tableau[tempx][tempy] %2 == tableau[positionx][positiony] %2 )) {
 			return poscible;
 		}
 	}
 }
 
-vector<int> peutmanger(vector<vector<int>> tableau, int directionx, int directiony, int positionx, int positiony) {
-	vector <int> = [-1, -1];
-	int tempx = positionx;
-	int tempy = positiony;
-	if ((tableau[tempx][tempy] > 0) and (tableau[tempx][tempy] % 2 != tableau[positionx][positiony] % 2)) {
-		tempx += directionx;
-		tempy += directiony;
-		if (tableau[tempx][tempy] == 0) {
-			poscible = [tempx, tempy];
-			rep = true;
+vector<vector<int>> optimisation(int x, int y, vector<vector<int>> tableau) {
+	vector<vector<int>> chemin;
+	vector<int> poscible(2, -1);
+	unsigned int max = 0;
+	if ((max < (optimisation(peutmangerdame(tableau, 1, 1, x, y)[0], peutmangerdame(tableau, 1, 1, x, y)[1], tableau)).size()) && (peutmangerdame(tableau, 1, 1, x, y) != poscible)) {
+		max = (optimisation(peutmangerdame(tableau, 1, 1, x, y)[0], peutmangerdame(tableau, 1, 1, x, y)[1], tableau)).size();
+		chemin[0] = peutmanger(tableau, 1, 1, x, y);
+	}
+	if ((max < (optimisation(peutmangerdame(tableau, -1, 1, x, y)[0], peutmangerdame(tableau, -1, 1, x, y)[1], tableau)).size()) && (peutmangerdame(tableau, -1, 1, x, y) != poscible)) {
+		max = (optimisationpion(peutmangerdame(tableau, -1, 1, x, y)[0], peutmangerdame(tableau, -1, 1, x, y)[1], tableau)).size();
+		chemin[0] = peutmanger(tableau, -1, 1, x, y);
+	}
+	if ((max < (optimisation(peutmangerdame(tableau, 1, -1, x, y)[0], peutmangerdame(tableau, 1, -1, x, y)[1], tableau)).size()) && (peutmangerdame(tableau, 1, -1, x, y) != poscible)) {
+		max = (optimisation(peutmangerdame(tableau, 1, -1, x, y)[0], peutmangerdame(tableau, 1, -1, x, y)[1], tableau)).size();
+		chemin[0] = peutmanger(tableau, 1, -1, x, y);
+	}
+	if ((max < (optimisation(peutmangerdame(tableau, -1, -1, x, y)[0], peutmangerdame(tableau, -1, -1, x, y)[1], tableau)).size()) && (peutmangerdame(tableau, -1, -1, x, y) != poscible)) {
+		max = (optimisation(peutmangerdame(tableau, -1, -1, x, y)[0], peutmangerdame(tableau, -1, -1, x, y)[1], tableau)).size();
+		chemin[0] = peutmanger(tableau, -1, -1, x, y);
+	}
+	if (chemin.size()==0) { return chemin; }
+	vector<vector<int>> a = optimisation(x + chemin[0][0], y + chemin[0][1], tableau);
+	chemin.insert(chemin.end(), a.begin(), a.end());
+	tableau[chemin[0][0]][chemin[0][1]] = -1;
+	return chemin;
+}
+
+
+
+vector<int> mvtmax(vector<vector<int>> tableau,int joueur) {
+	int x = 0;
+	int y = 0;
+	unsigned int opti = 0;
+	for (int i = 0; i < 10; i++) {
+		for (int k = 0; k < 10; k++) {
+			if (tableau[i][k]%2==joueur){
+				if (optimisation(i, k, tableau).size() > opti) {
+					x = i;
+					y = k;
+					opti = optimisation(i, k, tableau).size();
+				}
+			}
+		}
+	 }
+	vector<int> meilleur;
+	meilleur.push_back(x);
+	meilleur.push_back(y);
+	for(int i=0; i<9; i++){
+		for(int j=0; j<9; j++){
+			if(tableau[i][j]==-1){
+				tableau[i][j]=0;
+			}
 		}
 	}
+	return meilleur;
 }
---
+++ dames/dames/optimisation.h
@@ -0,0 +1,8 @@
+#include <vector>
+using namespace std;
+
+vector<int> mvtmax(vector<vector<int>>,int joueur);
+vector<vector<int>> optimisationpion(int, int, vector<vector<int>>);
+vector<vector<int>> optimisation(int, int);
+vector<int> peutmangerdame(vector<vector<int>>, int, int, int, int);
+vector<int> peutmanger(vector<vector<int>>, int, int, int, int);
